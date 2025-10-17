3267e1efc1885262e19840d3e77330152e45e618
Vicente Apaza <vicente.empres4@gmail.com>
2025-10-17 08:44:00 -0300

    app

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "a9c62cf5d2044651502301fe700257cc
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "637fe34fa5c76548963fd3020fa9da73",
+"flutter_bootstrap.js": "4887f972b2fe771a973f98ce80fd3d37",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
